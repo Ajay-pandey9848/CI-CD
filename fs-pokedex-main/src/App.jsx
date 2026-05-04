5fa3dd7390339c57fe7a518a6aa830360b1eb382
ajaypandey <pandeyajay2015@gmail.com>
2026-05-04 06:57:10 +0300

    fix ESLint: add React import, remove semicolons, delete unused var

--- fs-pokedex-main/src/App.jsx
+++ fs-pokedex-main/src/App.jsx
@@ -1,16 +1,17 @@
-import { Routes, Route, useMatch } from 'react-router-dom';
-import PokemonPage from './PokemonPage';
+import React from 'react'
+import { Routes, Route, useMatch } from 'react-router-dom'
+import PokemonPage from './PokemonPage'
 
 const App = () => {
-  const match = useMatch('/pokemon/:name');
-  const pokemonName = match ? match.params.name : null;
+  const match = useMatch('/pokemon/:name')
+  // const pokemonName = match ? match.params.name : null // removed unused variable
 
   return (
     <Routes>
       <Route path="/" element={<div>Home page (list of Pokémon goes here)</div>} />
       <Route path="/pokemon/:name" element={<PokemonPage />} />
     </Routes>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- fs-pokedex-main/src/PokemonPage.jsx
+++ fs-pokedex-main/src/PokemonPage.jsx
@@ -1,5 +1,5 @@
+import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
-import { useState, useEffect } from 'react'
 
 const formatName = (name) => name.replace('-', ' ').toUpperCase()
 
