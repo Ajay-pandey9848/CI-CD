c93e09f1e7a788b57a0112ac093395547a85266f
ajaypandey <pandeyajay2015@gmail.com>
2026-05-04 07:11:32 +0300

    corrected code

--- fs-pokedex-main/src/App.jsx
+++ fs-pokedex-main/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, useMatch } from 'react-router-dom'
 import PokemonPage from './PokemonPage'
 
 const App = () => {
+    // eslint-disable-next-line no-unused-vars
   const match = useMatch('/pokemon/:name')
   // pokemonName removed because it was unused
 
