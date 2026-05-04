9716e22b46aa23c401ad6eb48ff9cf968af00c96
ajaypandey <pandeyajay2015@gmail.com>
2026-05-04 08:12:57 +0300

    happy

--- fs-pokedex-main/jest.setup.js
+++ fs-pokedex-main/jest.setup.js
@@ -1,7 +1,7 @@
 /* global require, global */
 
 const { TextEncoder, TextDecoder } = require('util')
+require('whatwg-fetch')  // This adds fetch to global
 
 global.TextEncoder = TextEncoder
 global.TextDecoder = TextDecoder
-import 'whatwg-fetch';
