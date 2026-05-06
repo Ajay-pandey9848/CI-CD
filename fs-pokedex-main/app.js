7c6afe0291e6d4390b996f8479c69ba0f91ac1ae
ajaypandey <pandeyajay2015@gmail.com>
2026-05-06 07:16:47 +0300

    Fix ESLint extra semicolon errors

--- fs-pokedex-main/app.js
+++ fs-pokedex-main/app.js
@@ -7,9 +7,9 @@ app.use(express.static('dist'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-});;
-;
-const PORT = process.env.PORT || 8080;;
+});
+
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});;
+});
